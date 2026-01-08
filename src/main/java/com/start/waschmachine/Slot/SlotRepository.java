b32498bccca5ed3f48695c1579495fdb8bb98abc
unknown <seifeddinebenyaala424@gmail.com>
2026-01-09 00:06:57 +0100

    frontend added

--- src/main/java/com/start/waschmachine/Slot/SlotRepository.java
+++
@@ -1,8 +0,0 @@
-package com.start.waschmachine.Slot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface SlotRepository extends JpaRepository<Slot, Integer> {
-
-}
-
