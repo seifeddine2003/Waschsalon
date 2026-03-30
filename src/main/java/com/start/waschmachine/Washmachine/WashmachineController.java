02137aa3e8de7cd6598e582285b30b54ee6d7a2a
unknown <seifeddinebenyaala424@gmail.com>
2026-03-30 10:59:21 +0200

    fix: add CrossOrigin import to controllers

--- src/main/java/com/start/waschmachine/Washmachine/WashmachineController.java
+++ src/main/java/com/start/waschmachine/Washmachine/WashmachineController.java
@@ -1,6 +1,7 @@
 package com.start.waschmachine.Washmachine;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
