eb5a4702f07af143ad8531ad32c4cc53eb697c85
unknown <seifeddinebenyaala424@gmail.com>
2026-04-07 20:15:14 +0200

    password leaked

--- src/main/java/com/start/waschmachine/WaschmachineApplication.java
+++ src/main/java/com/start/waschmachine/WaschmachineApplication.java
@@ -2,8 +2,9 @@ package com.start.waschmachine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class WaschmachineApplication {
 
 	public static void main(String[] args) {
